f4c11afeb83a54f8ca0d5cd43d1e7d1b845924d6
ElisaFerrariNavarro <elisafn44@gmail.com>
2026-08-17 12:16:54 -0300

    web

--- 4_condicionais.php
+++ 4_condicionais.php
@@ -28,5 +28,13 @@ if($_SERVER['REQUEST_METHOD']=='POST') {
         <input type="password" name="senha" required><br>
         <button type="submit">Entrar</button>
     </form>
+    <?php
+    if (isset ($erro)) {
+    echo "<p style = 'color: red';>$erro</p>";
+    }
+    
+    ?>
+
+
 </body>
 </html>
