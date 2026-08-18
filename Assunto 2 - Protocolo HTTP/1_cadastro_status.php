432942c3fdebaf00c30e1340f33f0cd9eb47eace
ElisaFerrariNavarro <elisafn44@gmail.com>
2026-08-18 11:41:44 -0300

    http

---
+++ Assunto 2 - Protocolo HTTP/1_cadastro_status.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro - status codes</title>
+</head>
+<body>
+    <h1>Cadastro de alunos (com status codes)</h1> <\h1>
+
+
+<form method="post" action="">
+    <label for="nome">Nome:</label>
+    <input type="text" name="nome" required> <br><br>
+
+    <label for="idade">idade:</label>
+    <input type="text"  name="idade" required> <br><br>
+
+    <button type="submit"> Enviar<\button>
+</form>
+
+
+
+
+
+</body>
+</html>
