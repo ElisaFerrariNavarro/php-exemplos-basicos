adb8b69b0e5d11b22ab3f9295333cd3352c9a010
ElisaFerrariNavarro <elisafn44@gmail.com>
2026-09-01 09:07:01 -0300

    exercicio
    codigo de exemplo

--- Assunto 2 - Protocolo HTTP/1_cadastro_status.php
+++ Assunto 2 - Protocolo HTTP/1_cadastro_status.php
@@ -18,9 +18,23 @@
     <button type="submit"> Enviar<\button>
 </form>
 
-
-
-
-
 </body>
 </html>
+
+
+
+<?php
+
+if ($_SERVER ['REQUEST_METHOD'] == 'post') {
+    $nome = $_POST ['nome'];
+    $idade  = $_POST['idade'];
+}
+
+if ($nome == '' || $idade = '') {
+    http_response_code(400);
+    echo "<h2>Status 400 - Faltou preencher nome ou idade!</h2>";
+
+}
+
+
+    ?>
