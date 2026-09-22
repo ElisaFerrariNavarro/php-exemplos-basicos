5b455a2f60366f1f42418654977ec4041e0adb82
ElisaFerrariNavarro <elisafn44@gmail.com>
2026-09-22 11:48:19 -0300

    db

--- Assunto_5/9_conexao_bd.php
+++ Assunto_5/9_conexao_bd.php
@@ -1,3 +1,25 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "Senai@118";
+$dbname = "exercicio";
+
+
+try {
+
+//tenta criar uma conexão com o banco de dados 
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        throw new Exception("Falha na conexão: " . $conn->connect_error);
+    }
+    // Se tudo ocorrer bem e a conexão for estabelecida 
+    echo "Conexão realizada com sucesso!";
+} catch (Exception $e) {
+    echo "Erro ao conectar ao banco de dados" .$e->getMessage();
+}
+
+?>
 
 <!-- Para criar o BD -->
 <!-- CREATE DATABASE exercicio; -->
@@ -8,5 +30,7 @@
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL
 ); -->
+
+
 
 
